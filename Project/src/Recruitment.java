8b12224c908d373726b6d7908476ba86e085f7dc
Haoran Liu <hliu0072@student.monash.edu>
2020-05-17 06:15:03 +1000

    HaoranLiu_Feature2_Classes
    17/05/2020

---
+++ Project/src/Recruitment.java
@@ -0,0 +1,23 @@
+public class Recruitment {
+    private int recruitmentId;
+    private int candidateId;
+
+    public Recruitment() {
+    }
+
+    public int getRecruitmentId() {
+        return recruitmentId;
+    }
+
+    public void setRecruitmentId(int recruitmentId) {
+        this.recruitmentId = recruitmentId;
+    }
+
+    public int getCandidateId() {
+        return candidateId;
+    }
+
+    public void setCandidateId(int candidateId) {
+        this.candidateId = candidateId;
+    }
+}
